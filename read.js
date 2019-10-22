242a18c1bcea3e1146fd914c3449006bbb83ef75
Spencer Johnston <spencer.koh.johnston@gmail.com>
2019-10-22 13:53:16 -0500

    Added connection to DynamoDB

---
+++ read.js
@@ -0,0 +1,30 @@
+var AWS = require("aws-sdk");
+
+let awsConfig = {
+	"region" : "us-east-2",
+	"endpoint" : "http://dynamodb.us-east-2.amazonaws.com",
+	"accessKeyID" : "AKIAYJRLBTN5RQY2VJM3", "secretAccessKey" : "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"
+};
+
+AWS.config.update(awsConfig);
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+let fetchOneByKey = function() {
+	
+	var params = {
+		TableName: "Trauma_Toolkit",
+		Key: {
+			"ID" : "3"
+		}
+	};
+	
+	docClient.get(params, function (err, data) {
+		if (err) {
+			console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+		} 
+		else {
+			console.log ("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+		}
+	})
+}
