ce4073e6080d779273dab9ed0e634cbecf2b85fb
Spencer Johnston <spencer.koh.johnston@gmail.com>
2019-10-27 19:59:19 -0500

    Space Issue

--- read.js
+++ read.js
@@ -24,7 +24,7 @@ let fetchOneByKey = function() {
 			console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
 		} 
 		else {
-			console.log ("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+			console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
 		}
 	})
 }
