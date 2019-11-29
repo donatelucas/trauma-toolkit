1a0e690ec385d48be48b1db80201f4d12aeece30
Spencer Johnston <spencer.koh.johnston@gmail.com>
2019-11-29 17:47:29 -0600

    Added all queries

--- app/assets/scripts/json_app.js
+++ app/assets/scripts/json_app.js
@@ -40,16 +40,38 @@ function loadApp() {
 ///////////////////////////////////////////////////////
 
   function buildRapeTools(response) {
-      $(".rapetools-output").empty();
+      $(".rapetools-definition").empty();
+      $(".rapetools-mentalhealthservices").empty();
+      $(".rapetools-physicalhealthservices").empty();
+      $(".rapetools-reportingoptions").empty();
+      $(".rapetools-helpfullinks").empty();
       var $rapetools = response;
       $rapetools.forEach(function(item) {
         if (item !== null) {
-          var rapetools = item.Reporting_Options;
-          console.log(typeof rapetools);
-          rapetools = rapetools.replace(/,/g, "<br>")
+          var definition = item.Definition;
+          var mentalhealthservices = item.Mental_Health_Services;
+          var physicalhealthservices = item.Physical_Health_Services;
+          var reportingoptions = item.Reporting_Options;
+          var helpfullinks = item.Helpful_Links;
+          mentalhealthservices = mentalhealthservices.replace(/,/g, "<br>")
+          physicalhealthservices = physicalhealthservices.replace(/,/g, "<br>")
+          reportingoptions = reportingoptions.replace(/,/g, "<br>")
+          helpfullinks = helpfullinks.replace(/,/g, "<br>")
           var p = $("<p>");
-          p.html(rapetools);
-          $(".rapetools-output").append(p);
+          var q = $("<p>");
+          var r = $("<p>");
+          var s = $("<p>");
+          var t = $("<p>");
+          p.html(definition);
+          q.html(mentalhealthservices);
+          r.html(physicalhealthservices);
+          s.html(reportingoptions);
+          t.html(helpfullinks);
+          $(".rapetools-definition").append(p);
+          $(".rapetools-mentalhealthservices").append(q);
+          $(".rapetools-physicalhealthservices").append(r);
+          $(".rapetools-reportingoptions").append(s);
+          $(".rapetools-helpfullinks").append(t);
         }
       });
     }
@@ -62,6 +84,55 @@ function loadApp() {
     }
 
   getRapeTools();
+
+
+  //////////////////////////////////////////////////////
+
+    function buildSexualAbuseTools(response) {
+      $(".sexualabusetools-definition").empty();
+      $(".sexualabusetools-mentalhealthservices").empty();
+      $(".sexualabusetools-physicalhealthservices").empty();
+      $(".sexualabusetools-reportingoptions").empty();
+      $(".sexualabusetools-helpfullinks").empty();
+      var $sexualabusetools = response;
+      $sexualabusetools.forEach(function(item) {
+        if (item !== null) {
+          var definition = item.Definition;
+          var mentalhealthservices = item.Mental_Health_Services;
+          var physicalhealthservices = item.Physical_Health_Services;
+          var reportingoptions = item.Reporting_Options;
+          var helpfullinks = item.Helpful_Links;
+          mentalhealthservices = mentalhealthservices.replace(/,/g, "<br>")
+          physicalhealthservices = physicalhealthservices.replace(/,/g, "<br>")
+          reportingoptions = reportingoptions.replace(/,/g, "<br>")
+          helpfullinks = helpfullinks.replace(/,/g, "<br>")
+          var p = $("<p>");
+          var q = $("<p>");
+          var r = $("<p>");
+          var s = $("<p>");
+          var t = $("<p>");
+          p.html(definition);
+          q.html(mentalhealthservices);
+          r.html(physicalhealthservices);
+          s.html(reportingoptions);
+          t.html(helpfullinks);
+          $(".sexualabusetools-definition").append(p);
+          $(".sexualabusetools-mentalhealthservices").append(q);
+          $(".sexualabusetools-physicalhealthservices").append(r);
+          $(".sexualabusetools-reportingoptions").append(s);
+          $(".sexualabusetools-helpfullinks").append(t);
+        }
+      });
+    }
+
+    function getSexualAbuseTools() {
+    $.getJSON("SexualAbuseTools.json", function (response) {
+      console.log(response);
+      buildSexualAbuseTools(response);
+    });
+    }
+
+  getSexualAbuseTools();
 
 };
 $(document).ready(loadApp);
