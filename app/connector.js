01932ef80e43376e1ee91a123cedcfdd590dd9c2
Spencer Johnston <spencer.koh.johnston@gmail.com>
2019-11-29 16:59:42 -0600

    Revert "Removed old connector"
    This reverts commit b85020e2189fc520f310f997c8e2254d15dd9ad9.

---
+++ app/connector.js
@@ -0,0 +1,21 @@
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb+srv://sjohnston1:Password@trauma-toolkit-tef6r.mongodb.net/test?retryWrites=true&w=majority?authSource=admin"
+
+const dbName = 'Trauma-Toolkit'
+let db
+
+MongoClient.connect(url, { useNewUrlParser: true}, (err, client) => {
+
+	if (err) return console.log(err)
+
+	db = client.db(dbName)
+	console.log(`Connected MongoDB: ${url}`)
+	console.log(`Database: ${dbName}`)
+
+	var query = { Topic: "Rape Tools" };
+  	db.collection("Subjects").find(query).toArray(function(err, result) {
+    	if (err) throw err;
+    	console.log(result);
+	});
+
+})
